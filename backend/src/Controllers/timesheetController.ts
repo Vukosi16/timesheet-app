54de8434de246d7a3b397f7d91590f651568102a
Vukosi Mohlabini <vukosimohlabini16@gmail.com>
2026-07-28 22:07:00 +0200

    Completed admin timesheet routes

--- backend/src/Controllers/timesheetController.ts
+++ backend/src/Controllers/timesheetController.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 import prisma from "../lib/prisma";
-import { array } from "zod";
+import { ParamsDictionary } from 'express-serve-static-core';
 
 const createTimesheet = async(req: Request, res: Response) => {
     try{
@@ -178,12 +178,126 @@ const submitTimesheet = async(req: Request, res: Response) => {
     }
 }
 
+const getSubmittedTimesheets = async (req: Request, res: Response) => {
+    try {
+        const userId = req.userInfo?.userId;
+        if (!userId) {
+            return res.status(401).json({ message: "Unauthenticated user" });
+        }
+
+        const timesheets = await prisma.timesheet.findMany({
+            where: { stage: 'SUBMITTED' },
+            include: { timesheetEntry: true }
+        });
+
+        res.status(200).json({
+            message: "Timesheets retireved",
+            timesheets
+        })
+
+    } catch (e) {
+        console.log(e);
+        res.status(500).json({
+            error: `${e}`,
+            message : "Couldn't create a timesheet. Something went wrong"
+        })
+    }
+
+}
+
+interface ReviewBody {
+  decision: "APPROVE" | "REJECT";
+  adminMessage?: string;
+}
+
+interface ReviewParams extends ParamsDictionary {
+  id: string;
+}
+
+const reviewTimesheet = async(req: Request<ReviewParams, {}, ReviewBody, {}>, res: Response) => {
+   try {
+        const timesheetId = Number(req.params.id);
+        const { decision, adminMessage } = req.body;
+
+        const timesheet = await prisma.timesheet.findUnique({ where: { id: timesheetId }, include: { timesheetEntry: true } })
+        if (!timesheet){
+            return res.status(404).json({ message: "Timesheet not found" });
+        }  
+
+        if (timesheet.stage !== "SUBMITTED") {
+            return res.status(403).json({ message: "Timesheet has already been submitted" });
+        }
+
+         if (decision !== "APPROVE" && decision !== "REJECT") {
+            return res.status(400).json({ message: "Invalid decision value" });
+        }
+
+        let newTimesheet;
+        if (decision === "APPROVE"){
+            newTimesheet = await prisma.timesheet.update({ where: { id:timesheetId }, data: { stage: 'APPROVED'} })
+            return res.status(200).json({
+                message: "Timesheet approved",
+                newTimesheet
+            })
+        }
+
+        if (decision === "REJECT"){
+            newTimesheet = await prisma.timesheet.update({ where: { id:timesheetId }, data: { stage: 'STAGING', adminMessage} })
+            return res.status(200).json({
+                message: "Timesheet rejected",
+                adminMessage: adminMessage,
+                newTimesheet
+            })
+        }
+
+       
+
+   } catch (e) {
+        console.log(e);
+        res.status(500).json({
+            error: `${e}`,
+            message : "Couldn't create a timesheet. Something went wrong"
+        })
+   }
+
+}
+
+const markPaid = async(req: Request, res: Response) => {
+    try {
+        const timesheetId = Number(req.params.id);
+
+        const timesheet = await prisma.timesheet.findUnique({ where: { id: timesheetId }, include: { timesheetEntry: true } })
+        if (!timesheet){
+            return res.status(404).json({ message: "Timesheet not found" });
+        }  
+
+        if (timesheet.stage !== "APPROVED") {
+            return res.status(403).json({ message: "Timesheet must be approved before it can be marked as paid" });
+        }
+
+        const newTimesheet =  await prisma.timesheet.update({ where: { id:timesheetId }, data: { paid: true} })
+        return res.status(200).json({
+            message: "Marked as paid",
+            newTimesheet
+        })
+    } catch (e) {
+        console.log(e);
+        res.status(500).json({
+            error: `${e}`,
+            message : "Couldn't create a timesheet. Something went wrong"
+        })
+    }
+}
 
 
-export default  {
+
+export default {
     createTimesheet,
     deleteTimesheet,
     getCoachTimesheets,
     getTimesheetById,
-    submitTimesheet
+    submitTimesheet,
+    getSubmittedTimesheets,
+    reviewTimesheet,
+    markPaid
 }
--- backend/src/Routes/timesheetRoutes.ts
+++ backend/src/Routes/timesheetRoutes.ts
@@ -1,12 +1,16 @@
 import { Router } from 'express';
 import timesheetController from '../Controllers/timesheetController';  
 import authMiddleware from '../Middleware/authMiddleware';
+import adminMiddleware from '../Middleware/adminMiddleware';
 
 const router = Router();
 
 router.get('/timesheets', authMiddleware, timesheetController.getCoachTimesheets)
 router.get('/timesheet/:id', authMiddleware, timesheetController.getTimesheetById)
+router.get('/submitted', authMiddleware, adminMiddleware, timesheetController.getSubmittedTimesheets)
 router.post('/create', authMiddleware,timesheetController.createTimesheet);
+router.post('/review/:id', authMiddleware, adminMiddleware, timesheetController.reviewTimesheet)
+router.post('/paid/:id', authMiddleware, adminMiddleware, timesheetController.markPaid)
 router.patch('/submit/:id', authMiddleware, timesheetController.submitTimesheet);
 router.delete('/delete/:id', authMiddleware, timesheetController.deleteTimesheet);
 
